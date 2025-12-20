dee828c2704b7748012890ce6549b63dcf91dc9f
leeseungjun <cjstkrak@gmail.com>
2025-12-20 20:30:33 +0900

    fix: null 일경우 예외처리

--- src/domain/chats/chats.service.ts
+++ src/domain/chats/chats.service.ts
@@ -57,6 +57,9 @@ export class ChatsService {
     const currentUser = await this.usersRepository.findOne({
       where: { id: userId },
     });
+    if (!currentUser) {
+      throw new BadRequestException('user가 존재하지 않습니다.');
+    }
 
     const newRoom = this.chatRoomsRepository.create();
     newRoom.user1 = currentUser;
@@ -236,6 +239,10 @@ export class ChatsService {
     const sender = await this.usersRepository.findOne({
       where: { id: senderId },
     });
+
+    if (!sender) {
+      throw new BadRequestException('user가 존재하지 않습니다.');
+    }
 
     // 메시지 저장
     const message = this.messagesRepository.create();
