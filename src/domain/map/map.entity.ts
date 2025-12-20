485f2d5d8d8468be8a92f5196305e28b48416983
yohan <icb1696@gmail.com>
2025-12-20 17:25:58 +0900

    Fix: user, map 관계 설정

--- src/domain/map/map.entity.ts
+++ src/domain/map/map.entity.ts
@@ -1,8 +1,15 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 import { BaseTimestampEntity } from '../../common/base.entity.js';
+import { User } from '../users/user.entity';
 
 @Entity('maps')
 export class Map extends BaseTimestampEntity {
-  @Column({ type: 'varchar', length: 500, nullable: false })
+  @Column({ type: 'varchar', length: 100, nullable: false })
+  name: string;
+
+  @Column({ type: 'text', nullable: false })
   imageUrl: string;
+
+  @OneToMany(() => User, (user) => user.currentMap)
+  users: User[];
 }
